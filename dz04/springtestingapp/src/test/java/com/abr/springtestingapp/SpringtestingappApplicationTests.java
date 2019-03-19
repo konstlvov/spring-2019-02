f5f6d6e7f654561f1fb945c968719f44efe8f81b
Konstantin Lvov <k.lvov@abr.ru>
2019-03-19 15:50:24 +0300

    Настроена обвязка для тестов (если в приложении есть spring shell, то надо настраивать обвязку таким образом, чтобы шелл не запускался во время mvn test, а то на нем все будет зависать)

--- dz04/springtestingapp/src/test/java/com/abr/springtestingapp/SpringtestingappApplicationTests.java
+++ dz04/springtestingapp/src/test/java/com/abr/springtestingapp/SpringtestingappApplicationTests.java
@@ -4,13 +4,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertTrue;
+import org.springframework.shell.jline.InteractiveShellApplicationRunner;
+import org.springframework.shell.jline.ScriptShellApplicationRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(properties = {
+    InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED + "=false",
+    ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false"
+})
 public class SpringtestingappApplicationTests {
 
 	@Test
 	public void contextLoads() {
+            assertTrue(true);
 	}
 
 }
