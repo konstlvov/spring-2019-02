64bb27ce70314a679c46d80ac45226bef7b03733
Konstantin Lvov <k.lvov@abr.ru>
2019-03-29 15:35:21 +0300

    Отформатирована табуляция: теперь отступ будет везде по 2 пробела и без табуляции, как в Google

--- dz06/src/main/java/ru/otus/spring06/dao/AuthorDaoJdbc.java
+++ dz06/src/main/java/ru/otus/spring06/dao/AuthorDaoJdbc.java
@@ -19,42 +19,41 @@ import ru.otus.spring06.domain.Person;
 @Repository
 public class AuthorDaoJdbc implements IAuthorDao {
 
-    private final NamedParameterJdbcOperations jo;
+  private final NamedParameterJdbcOperations jo;
 
-    public AuthorDaoJdbc(NamedParameterJdbcOperations jdbcOperations) {
-        jo = jdbcOperations;
-    }
+  public AuthorDaoJdbc(NamedParameterJdbcOperations jdbcOperations) {
+    jo = jdbcOperations;
+  }
     
   @Override
   public int getAuthorCount(){
-      return jo.queryForObject("select count(*) from Author", Collections.EMPTY_MAP, Integer.class);
-  };
+    return jo.queryForObject("select count(*) from Author", Collections.EMPTY_MAP, Integer.class);
+  }
   
   @Override
   public void insertAuthor(Author a){
-       HashMap<String, Object> params = new HashMap<>();
-       params.put("authorId", a.getId());
-       params.put("authorName", a.getName());
-       jo.update("insert into Author (AuthorId, AuthorName) values (:authorId, :authorName)", params);
-  };
+    HashMap<String, Object> params = new HashMap<>();
+    params.put("authorId", a.getId());
+    params.put("authorName", a.getName());
+    jo.update("insert into Author (AuthorId, AuthorName) values (:authorId, :authorName)", params);
+  }
 
   public static class AuthorMapper implements RowMapper<Author> {
-      @Override
-      public Author mapRow(ResultSet resultSet, int rowNum) throws SQLException {
-          int id = resultSet.getInt("AuthorID");
-          String name = resultSet.getString("AuthorName");
-          return new Author(id, name);
-      }
+    @Override
+    public Author mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+      int id = resultSet.getInt("AuthorID");
+      String name = resultSet.getString("AuthorName");
+      return new Author(id, name);
+    }
   }
   
   public Author getById(int id){
-        HashMap<String, Object> params = new HashMap<>();
-        params.put("authorId", id);
-        return jo.queryForObject("select * from Author where AuthorId = :authorId", params, new AuthorDaoJdbc.AuthorMapper());
-  };
-
+    HashMap<String, Object> params = new HashMap<>();
+    params.put("authorId", id);
+    return jo.queryForObject("select * from Author where AuthorId = :authorId", params, new AuthorDaoJdbc.AuthorMapper());
+  }
   
   public List<Author> getAllAuthors(){
-      return jo.getJdbcOperations().query("select * from Author", new AuthorMapper());
-  };
+    return jo.getJdbcOperations().query("select * from Author", new AuthorMapper());
+  }
 }
