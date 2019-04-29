f48808b8be81278ddc93acf0391e65f9fc20e9e7
Konstantin Lvov <k.lvov@abr.ru>
2019-04-29 14:34:41 +0300

    Сделан метод Delete. Можно считать, что REST api готово и работает

--- spring-io-guides/gs-accessing-mongodb-data-rest/complete/src/main/java/hello/BookController.java
+++ spring-io-guides/gs-accessing-mongodb-data-rest/complete/src/main/java/hello/BookController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,6 +62,10 @@ public class BookController {
         return null;
       }
     }
-
+    
+    @DeleteMapping("/books/{id}")
+    public void deleteBook(@PathVariable("id") String id) {
+      bookRepository.findById(id).ifPresent(book -> bookRepository.delete(book));
+    }
     
 }
