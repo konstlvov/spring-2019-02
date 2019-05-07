e6c103403b557a41de434e06ee53fef3b22e9972
Konstantin Lvov <k.lvov@abr.ru>
2019-05-07 14:22:34 +0300

    Заготовка для фронтэнда (angular)

---
+++ dz15-spring-rest-angular/angular-frontend/app.js
@@ -0,0 +1,39 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+
+var apiRouter = require('./routes/book');
+
+var app = express();
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://vm-dit-lvov:8200/mean-angular6', { promiseLibrary: require('bluebird') })
+  .then(() =>  console.log('connection to mongodb://vm-dit-lvov:8200/mean-angular6 successful'))
+  .catch((err) => console.error(err));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'dist/mean-angular6')));
+app.use('/', express.static(path.join(__dirname, 'dist/mean-angular6')));
+app.use('/api', apiRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send(err.status);
+});
+
+module.exports = app;
