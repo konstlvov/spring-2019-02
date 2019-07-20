983b61ff2a9efc2eaa81a85e440e4734e1d9f9ca
Konstantin A. Lvov <konst.lvov@gmail.com>
2019-07-20 14:54:34 +0300

    Улучшаю типизацию TypeScript

--- dz15-spring-rest-angular/angular-frontend/src/app/api.service.ts
+++ dz15-spring-rest-angular/angular-frontend/src/app/api.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+
 import { Observable, of, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap, map } from 'rxjs/operators';
@@ -43,7 +44,7 @@ export class ApiService {
     return body || { };
   }  
 
-  getBooks(): Observable<any> {
+  getBooks(): Observable<IBook[]> {
     return this.http.get(apiUrl, httpOptions).pipe(
       map(this.extractBooklistData),
       catchError(this.handleError));
---
+++ dz15-spring-rest-angular/angular-frontend/types/books/index.d.ts
@@ -0,0 +1,11 @@
+interface IBook {
+  _id: string;
+  isbn: string;
+  title: string;
+  author: string;
+  description: string;
+  published_year: string;
+  publisher: string;
+  updated_date: Date;
+}
+
