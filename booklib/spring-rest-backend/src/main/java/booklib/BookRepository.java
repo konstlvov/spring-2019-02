5b2571e0a0d93509c4d22cc55b9dcc4dbdf860c1
Konstantin A. Lvov <konst.lvov@gmail.com>
2019-07-21 19:20:27 +0300

    Убраны комментарии для аннотаций, относящиеся к @RepositoryRestResource

--- booklib/spring-rest-backend/src/main/java/booklib/BookRepository.java
+++ booklib/spring-rest-backend/src/main/java/booklib/BookRepository.java
@@ -2,7 +2,5 @@ package booklib;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-// collectionResourceRel - имя коллекции в mongodb
-// booklist - api endpoint (http://localhost:8080/booklist)
 public interface BookRepository extends ReactiveMongoRepository<Book, String> {
 }
