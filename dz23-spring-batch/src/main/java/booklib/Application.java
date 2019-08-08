b534a1b450b956c7c92f32b0bf1714f1924e0b69
Konstantin Lvov <k.lvov@abr.ru>
2019-08-08 17:54:51 +0300

    ДЗ по занятию 23: Spring batch. Реализовано конвертация данных Oracle -> MongoDB и обратно MongoDB -> Oracle. Задания можно запустить через Spring shell.

---
+++ dz23-spring-batch/src/main/java/booklib/Application.java
@@ -0,0 +1,20 @@
+package booklib;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+    }
+}
