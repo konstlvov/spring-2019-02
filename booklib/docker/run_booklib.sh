621aea0cc0f0cb053a31d280512fea97769ab2e2
Konstantin Lvov <k.lvov@abr.ru>
2019-08-22 17:40:55 +0300

    Добавлены батники для запуска и остановки контейнера rabbitmq

--- booklib/docker/run_booklib.sh
+++ booklib/docker/run_booklib.sh
@@ -3,5 +3,5 @@
 # to view application output in console, run it without -d switch:
 #docker run --name booklib -p 8080:8080 booklib:0.1.0
 
-docker run --name booklib -d -p 8080:8080 booklib:0.1.0
+docker run --rm --name booklib -d -p 8080:8080 booklib:0.1.0
 
